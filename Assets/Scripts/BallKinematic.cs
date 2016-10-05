a330641aa4df3516a2b9590c129a87de96ca4aba
aspieldenner <aespieldenner@gmaill.com>
2016-10-04 22:24:24 -0400

    Pong
    Pong

---
+++ Assets/Scripts/BallKinematic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallKinematic : MonoBehaviour {
+
+	float ballSpeed = 10.0f;
+
+
+	void Start() {
+
+	}
+
+
+	void Update () {
+
+		//we are using the transform component to move the object, there are many more options that can be found here:
+		//http://docs.unity3d.com/ScriptReference/Transform.Translate.html
+		//http://docs.unity3d.com/ScriptReference/Vector3.html
+
+		//also note the use of a time function/method
+		//http://docs.unity3d.com/ScriptReference/Time-deltaTime.html
+
+		//		transform.Translate(Vector2.left * ballSpeed*Time.deltaTime);
+		transform.Translate (new Vector2(-0.5f, -0.5f)* ballSpeed*Time.deltaTime);
+
+	}
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		rigidbody.AddForce(Vector3.Reflect(direction, 
+			collisionInfo.contacts[0].normal) * mag,   
+			ForceMode.Impulse);
+	}
+}
---
+++ Assets/Scripts/PaddleRight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleRight : MonoBehaviour {
+
+	public float paddleSpeed = .7f;
+
+
+	private Vector3 playerPos = new Vector3 (7.92f, -7.92f, 0);
+
+	void Update () 
+	{
+		float yPos = transform.position.y + (Input.GetAxis("Vertical") * paddleSpeed); //allows the player to control the paddle with the L and R arrow keys
+		playerPos = new Vector3 (7.92f, Mathf.Clamp (yPos, -4f, 4f), 0f); //limits how far the paddle can slide
+		transform.position = playerPos;
+
+	}
+}
